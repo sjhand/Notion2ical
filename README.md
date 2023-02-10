1863cad74284bc16186f79b90738b4dc31395d1a
Sjoerd <sjoerd@webswing.nl>
2023-02-10 20:08:43 +0100

    Added project files

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # Notion2ical
+
 This is a simple PHP script streaming Notion database items/pages with a date into the iCalendar format.
+
+## Prerequisites
+
+* PHP ^8.1
+* Composer
+
+## Installation
+
+Install dependencies through composer.
+
+```bash
+composer install
+```
+Create an .env file from the example, and set up the variables explained below.
+
+```bash
+cp .env.example .env
+```
+Set up a [Notion integration](https://developers.notion.com/docs/create-a-notion-integration) to receive an API key.
+
+The iCal feed will be accessible through `https://www.yourdomain.com/index.php?k=<SECRET_KEY>`.
+
+Make sure you add the key in the 'k' GET variable.
+
+## Environment Variables
+
+To run this script, you will need to add the following environment variables to your .env file
+
+`SECRET_KEY` Security key preventing public access
+
+`NOTION_API_KEY` Notion API key
+
+`NOTION_DB_ID` Notion database ID
+
+`NOTION_DATE_PROPERTY_NAME` Notion property name containing the task date
+
+`NOTION_STATUS_PROPERTY_NAME` Notion property name containing the status
+
+`NOTION_EXCLUDE_STATUS` Notion status to exclude in the iCal feed
---
+++ index.php
@@ -0,0 +1,92 @@
+<?php
+
+use Eluceo\iCal\Domain\Entity\Calendar;
+use Eluceo\iCal\Domain\Entity\Event;
+use Eluceo\iCal\Domain\ValueObject\Date;
+use Eluceo\iCal\Domain\ValueObject\SingleDay;
+use Eluceo\iCal\Presentation\Factory\CalendarFactory;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+
+require 'vendor/autoload.php';
+
+// Load the .env
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+if (!isset($_GET['k']) || $_GET['k'] !== $_ENV['SECRET_KEY']) {
+    header('HTTP/1.0 403 Forbidden');
+    echo 'access denied';
+    exit();
+}
+
+// Set up the Notion API call
+$client = new Client();
+try {
+    $response = $client->request('POST', 'https://api.notion.com/v1/databases/' . $_ENV['NOTION_DB_ID'] . '/query', [
+        'body' => json_encode([
+            'page_size' => 100,
+            'filter' => [
+                'and' => [
+                    [
+                        'property' => $_ENV['NOTION_DATE_PROPERTY_NAME'],
+                        'date' => [
+                            'is_not_empty' => true,
+                        ],
+                    ],
+                    [
+                        'property' => $_ENV['NOTION_STATUS_PROPERTY_NAME'],
+                        'status' => [
+                            'does_not_equal' => $_ENV['NOTION_EXCLUDE_STATUS'],
+                        ]
+                    ]
+                ],
+            ],
+            'sorts' => [
+                [
+                    'property' => 'Date',
+                    'direction' => 'descending',
+                ]
+            ],
+        ]),
+        'headers' => [
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer ' . $_ENV['NOTION_API_KEY'],
+            'Content-Type' => 'application/json',
+            'Notion-Version' => '2022-06-28',
+        ],
+    ]);
+} catch (GuzzleException $e) {
+    die($e->getMessage());
+}
+
+$result = json_decode($response->getBody(), false);
+
+if (null !== $result) {
+    $events = [];
+    foreach ($result->results as $item) {
+        $date = $item->properties->{$_ENV['NOTION_DATE_PROPERTY_NAME']}->date->start;
+        $title = $item->properties->Name->title[0]->text->content;
+        // Create an event
+        $event = new Event();
+        $event->setSummary($title);
+        $event->setDescription($item->url);
+        $event->setOccurrence(
+            new SingleDay(
+                new Date(
+                    DateTimeImmutable::createFromFormat('Y-m-d', $date)
+                )
+            )
+        );
+        $events[] = $event;
+    }
+
+    // Create calendar
+    $calendar = new Calendar($events);
+    $componentFactory = new CalendarFactory();
+    $calendarComponent = $componentFactory->createCalendar($calendar);
+
+    header('Content-Type: text/calendar; charset=utf-8');
+    header('Content-Disposition: attachment; filename="cal.ics"');
+    echo $calendarComponent;
+}
